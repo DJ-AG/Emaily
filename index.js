235b6b120a6b7536ff1c5c005f332f102bd789ae
DJ-AG <alekseigolovanov89@gmail.com>
2022-03-22 12:32:13 +0200

    updated google auth

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
+
+  require("./services/passport")
+
 const app = express();
 
-// PORT
+  require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
-
-app.listen(PORT);
+    app.listen(PORT);
